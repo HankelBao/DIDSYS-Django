8ae25709300353062870a3df77a41aad53d64f20
HankelBao <hankelbao@outlook.com>
2017-08-09 12:08:16 +0800

    having new ideas now

--- SFLSDID/urls.py
+++ SFLSDID/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from DID import views as views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
 ]
