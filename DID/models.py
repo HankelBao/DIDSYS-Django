ae111e880ba4b10dd726d83e9dd0818365f1a6c6
HankelBao <hankelbao@outlook.com>
2017-08-10 05:29:50 +0800

    define the db django is really awesome is database management

--- DID/models.py
+++ DID/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Clas(models.Model):
+    name = models.TextField()
+    is_service = models.BooleanField()
+
+
+class Subject(models.Model):
+    name = models.TextField()
+    full_score = models.IntegerField()
+    is_service = models.BooleanField()
+
+
+class Scorer(models.Model):
+    name = models.TextField()
+    subjects = models.ManyToManyField(Subject)
+    clases = models.ManyToManyField(Clas)
+    is_service = models.BooleanField()
+
+
+class Record(models.Model):
+    datetime = models.DateTimeField()
+    clas = models.ForeignKey(Clas)
+    subject = models.ForeignKey(Subject)
+    scorer = models.ForeignKey(Scorer)
