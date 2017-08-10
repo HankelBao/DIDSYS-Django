55c3e07857ed35a49bf1274b28fb81fe4df2dee2
HankelBao <hankelbao@outlook.com>
2017-08-10 11:16:22 +0800

    temp

--- DID/views.py
+++ DID/views.py
@@ -4,12 +4,25 @@ from .models import Clas
 from .models import Scorer
 from .models import Record
 
+
 def index(request):
     content = {}
-    list = Subject.objects.all()
+    subjects = Subject.objects.all()
+    clases = Clas.objects.all()
 
-    content['scoreboard'] = [[0 for i in range(6)] for j in range(6)]
-    content['scoreboard'][5][0] = '#'
+    content['scoreboard_size_x'] = len(subjects)
+    content['scoreboard_size_y'] = len(clases)
+    content['scoreboard'] = [
+        [0 for i in range(len(subjects) + 1)] for j in range(len(clases) + 1)]
+    content['scoreboard'][0][0] = '#'
+    x = 0
+    for subject in subjects:
+        x += 1
+        content['scoreboard'][0][x] = subject.name
+    y = 0
+    for clas in clases:
+        y += 1
+        content['scoreboard'][y][0] = clas.name
     #a = content['scoreboard']
     # a[0][0] = '#'
     return render(request, 'index.html', content)
