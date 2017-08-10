996ac7437c7f41caf43c36694f5f98a7628530d6
HankelBao <hankelbao@outlook.com>
2017-08-10 10:24:27 +0800

    pylint is bad in django although pylink-django solves a little bit, but auto-completion in models is bad

--- DID/views.py
+++ DID/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import Subject
+from .models import Clas
+from .models import Scorer
+from .models import Record
 
 def index(request):
+    content = {}
+    list = Subject.objects.all()
 
-    return render(request, 'index.html')
+    content['scoreboard'] = [[0 for i in range(6)] for j in range(6)]
+    content['scoreboard'][5][0] = '#'
+    #a = content['scoreboard']
+    # a[0][0] = '#'
+    return render(request, 'index.html', content)
 # Create your views here.
